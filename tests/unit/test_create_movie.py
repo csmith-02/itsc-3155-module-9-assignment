57791c9feda5ddcfc504cb6ef06707c95e58b67d
Jacob Taylor <jacobjtaylor0@gmail.com>
2023-10-24 20:18:33 -0400

    added tests for feature 2

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,13 @@
-# TODO: Feature 2
+from src.repositories.movie_repository import get_movie_repository
+
+def test_create_movie():
+    # Get the movie repository
+    singleton = get_movie_repository()
+    singleton.clear_db()
+
+    #Create movie and test to see if created successfully
+    movie1 = singleton.create_movie('Test-movie', 'Test-director', '5')
+    assert movie1.title == 'Test-movie'
+    assert movie1.director == 'Test-director'
+    assert movie1.rating == '5'
+
