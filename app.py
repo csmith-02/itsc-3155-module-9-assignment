c5c2a3e1c1dbc5600e8aaef8b9df0bc29a8fd2d6
Jacob Taylor <jacobjtaylor0@gmail.com>
2023-10-27 13:46:52 -0400

    Added Feature 2 Correctly

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, render_template, abort
+from flask import Flask, abort, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -7,9 +7,11 @@ app = Flask(__name__)
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
 
+
 @app.get('/')
 def index():
     return render_template('index.html')
+
 
 @app.get('/movies')
 def list_all_movies():
@@ -17,9 +19,11 @@ def list_all_movies():
     movies = movie_repository.get_all_movies()
     return render_template('list_all_movies.html', list_movies_active=True, movies=movies)
 
+
 @app.get('/movies/new')
 def create_movies_form():
     return render_template('create_movies_form.html', create_rating_active=True)
+
 
 @app.post('/movies')
 def create_movie():
@@ -36,25 +40,40 @@ def create_movie():
     # After creating the movie in the database, we redirect to the list all movies page
     return redirect('/movies')
 
+
+
 @app.get('/movies/search')
 def search_movies():
     # TODO: Feature 3
     return render_template('search_movies.html', search_active=True)
 
+
 @app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
-    # TODO: Feature 4
-    return render_template('get_single_movie.html')
+    movie = movie_repository.get_movie_by_id(movie_id)
+    return render_template('get_single_movie.html', movie=movie)
+
 
 @app.get('/movies/<int:movie_id>/edit')
 def get_edit_movies_page(movie_id: int):
-    return render_template('edit_movies_form.html')
+    movie = movie_repository.get_movie_by_id(movie_id)
+    return render_template('edit_movies_form.html', movie=movie)
+
 
 @app.post('/movies/<int:movie_id>')
 def update_movie(movie_id: int):
-    # TODO: Feature 5
+    global movie_repository
+    title = request.form.get('title')
+    director = request.form.get('director')
+    rating = request.form.get('rating')
+
+    if title is None or director is None or rating is None or int(rating) > 5 or int(rating) < 1:
+        abort(400)
+
+    movie_repository.update_movie(movie_id, title, director, rating)
     # After updating the movie in the database, we redirect back to that single movie page
     return redirect(f'/movies/{movie_id}')
+
 
 @app.post('/movies/<int:movie_id>/delete')
 def delete_movie(movie_id: int):
